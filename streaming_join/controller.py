b2ced90b5eeb51602a85093fe233f3668057f5a7
vagrant <vikas.natesh@gmail.com>
2018-09-17 06:55:08 +0000

    initial commit

---
+++ streaming_join/controller.py
@@ -0,0 +1,123 @@
+#!/usr/bin/env python2
+import argparse
+import grpc
+import os
+import sys
+import random
+from time import sleep
+
+sys.path.append(
+    os.path.join(os.path.dirname(os.path.abspath(__file__)),
+                 '../../utils/'))
+import p4runtime_lib.bmv2
+from p4runtime_lib.switch import ShutdownAllSwitchConnections
+import p4runtime_lib.helper
+
+
+def writeStaticTable(p4info_helper, switch):
+
+    hosts = [('10.0.1.1', "00:00:00:00:01:01", 1), ('10.0.1.2', "00:00:00:00:01:02", 2), ('10.0.1.3', "00:00:00:00:01:03", 3)]
+
+    table_entry = p4info_helper.buildTableEntry(
+        table_name="MyIngress.ipv4_lpm",
+        default_action=True,
+        action_name="MyIngress.drop",
+        action_params={ }
+        )
+    switch.WriteTableEntry(table_entry)
+
+    for h in hosts:
+        table_entry = p4info_helper.buildTableEntry(
+            table_name="MyIngress.ipv4_lpm",
+            match_fields={
+                "hdr.ipv4.dstAddr": (h[0], 32)
+            },
+            action_name="MyIngress.ipv4_forward",
+            action_params={
+                "dstAddr": h[1],
+                "port": h[2]
+            })
+        switch.WriteTableEntry(table_entry)
+
+
+def readTableRules(p4info_helper, sw):
+    """
+    Reads the table entries from all tables on the switch.
+    :param p4info_helper: the P4Info helper
+    :param sw: the switch connection
+    """
+    print '\n----- Reading tables rules for %s -----' % sw.name
+    for response in sw.ReadTableEntries():
+        for entity in response.entities:
+            entry = entity.table_entry
+            # TODO For extra credit, you can use the p4info_helper to translate
+            #      the IDs in the entry to names
+            table_name = p4info_helper.get_tables_name(entry.table_id)
+            print '%s: ' % table_name,
+            for m in entry.match:
+                print p4info_helper.get_match_field_name(table_name, m.field_id),
+                print '%r' % (p4info_helper.get_match_field_value(m),),
+            action = entry.action.action
+            action_name = p4info_helper.get_actions_name(action.action_id)
+            print '->', action_name,
+            for p in action.params:
+                print p4info_helper.get_action_param_name(action_name, p.param_id),
+                print '%r' % p.value,
+            print
+
+
+def printGrpcError(e):
+    print "gRPC Error:", e.details(),
+    status_code = e.code()
+    print "(%s)" % status_code.name,
+    traceback = sys.exc_info()[2]
+    print "[%s:%d]" % (traceback.tb_frame.f_code.co_filename, traceback.tb_lineno)
+
+def main(p4info_file_path, bmv2_file_path):
+    # Instantiate a P4Runtime helper from the p4info file
+    p4info_helper = p4runtime_lib.helper.P4InfoHelper(p4info_file_path)
+
+    # Create a switch connection object for s1
+    # this is backed by a P4Runtime gRPC connection.
+    # Also, dump all P4Runtime messages sent to switch to given txt files.
+    s1 = p4runtime_lib.bmv2.Bmv2SwitchConnection(
+        name='s1',
+        address='127.0.0.1:50051',
+        device_id=0,
+        proto_dump_file='logs/s1-p4runtime-requests.txt')
+
+    # Send master arbitration update message to establish this controller as
+    # master (required by P4Runtime before performing any other write operation)
+    s1.MasterArbitrationUpdate()
+
+    # Install the P4 program on the switch
+    s1.SetForwardingPipelineConfig(p4info=p4info_helper.p4info,
+                                   bmv2_json_file_path=bmv2_file_path)
+    print "Installed P4 Program using SetForwardingPipelineConfig on s1"
+
+    # write match rows into table
+    writeStaticTable(p4info_helper, switch=s1)
+
+    # read table entries from s1
+    readTableRules(p4info_helper, s1)
+    ShutdownAllSwitchConnections()
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='P4Runtime Controller')
+    parser.add_argument('--p4info', help='p4info proto in text format from p4c',
+                        type=str, action="store", required=False,
+                        default='./build/streaming_join.p4info')
+    parser.add_argument('--bmv2-json', help='BMv2 JSON file from p4c',
+                        type=str, action="store", required=False,
+                        default='./build/streaming_join.json')
+    args = parser.parse_args()
+
+    if not os.path.exists(args.p4info):
+        parser.print_help()
+        print "\np4info file not found: %s\nHave you run 'make'?" % args.p4info
+        parser.exit(1)
+    if not os.path.exists(args.bmv2_json):
+        parser.print_help()
+        print "\nBMv2 JSON file not found: %s\nHave you run 'make'?" % args.bmv2_json
+        parser.exit(1)
+    main(args.p4info, args.bmv2_json)
